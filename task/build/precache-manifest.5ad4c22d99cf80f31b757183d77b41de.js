887f11aab82b967a1426b1dda6efe7781a456b7e
DImka <MashinaDonnu>
2020-10-16 14:27:23 +0300

    added build folder

---
+++ task/build/precache-manifest.5ad4c22d99cf80f31b757183d77b41de.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b405077916cd9ae27a2d73fe71d8b652",
+    "url": "/index.html"
+  },
+  {
+    "revision": "287574fde0c367233ede",
+    "url": "/static/css/main.0d5529ff.chunk.css"
+  },
+  {
+    "revision": "dfc293735533cd24db87",
+    "url": "/static/js/2.149287fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.149287fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "287574fde0c367233ede",
+    "url": "/static/js/main.7dbd0185.chunk.js"
+  },
+  {
+    "revision": "afd9fccc04eb1a18eb43",
+    "url": "/static/js/runtime-main.0c7a2f62.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/static/media/menu icon.6f6bef28.svg"
+  },
+  {
+    "revision": "f8d2af43afb520bacb3d6656d7893186",
+    "url": "/static/media/mobile-header.f8d2af43.png"
+  }
+]);
